b2687fafabff2a3442195cbefaf796ac616ba97a
Anthony-Szol <szolanthony@gmail.com>
2024-04-05 09:24:44 -0700

    Commit

---
+++ project_sql/4_top_paying_skills.sql
@@ -0,0 +1,7 @@
+/*
+Question: What are the top skills based on salary?
+- Look at the average salary associated with each skill for Data Analyst positions.
+- Focuses on roles with specificed salaries, regardless of location.
+- Why? It reveals the most financially rewarding skills for Data Analyst.
+*/
+
